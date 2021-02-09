db72b58c6ed1bec4b51c9459ed9583f943747502
Fujiph <59277980+Fujiph@users.noreply.github.com>
2021-02-09 14:13:41 +0700

    V1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ItemTable from "./ItemTable";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; 
 
 function App() {
   //add useState for all state variables
@@ -41,17 +41,23 @@ function App() {
         </div>
 
         <button className="button is-primary is-fullwidth">Submit</button>
-
+        
+        
         <div className="mb-4"></div>
 
         {/* display tables for all persons */}
         <p className="is-4 title has-text-centered">Person List</p>
+        const ItemTable = (props) {
+
+        }
         {/* sample table */}
+
         <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        <p>Aueboon Ruanekamfu 620612170</p>
       </div>
     </div>
   );
 }
+
 
 export default App;
